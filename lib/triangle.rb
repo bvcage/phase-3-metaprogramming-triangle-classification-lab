79218c84783622ca42ef1c3330f280ac3ab02ec0
Bailey Cage <bailey.cage@gmail.com>
2022-09-20 20:04:30 -0400

    commit tree 1e518234bfb2aac2f47059f24fba88e1b4b602dc

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  
+  def initialize(side_a, side_b, side_c)
+    if side_a <= 0 || side_b <= 0 || side_c <= 0 then raise TriangleError end
+    if side_a + side_b <= side_c then raise TriangleError end
+    if side_a + side_c <= side_b then raise TriangleError end
+    if side_b + side_c <= side_a then raise TriangleError end
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if (@side_a == @side_b) && (@side_b == @side_c) then :equilateral
+    elsif (@side_a == @side_b) || (@side_b == @side_c) || (@side_a == @side_c) then :isosceles
+    else :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
